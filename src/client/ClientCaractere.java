4ea4a7f87baf650cfb942138975792e8911eee63
bassirou ngom <bassiroungom@esp.sn>
2022-06-13 11:52:29 +0000

    client/serveur caractere

---
+++ src/client/ClientCaractere.java
@@ -0,0 +1,30 @@
+package client;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ClientCaractere {
+    public static void main(String[] args) {
+        try {
+            // Etablissement de la connexion
+            Socket c = new Socket("localhost", 8080);
+
+            // transmission
+            InputStream is = c.getInputStream();
+            BufferedInputStream bif = new BufferedInputStream(is);
+            DataInputStream dif = new DataInputStream(bif);
+
+            String msg = dif.readUTF();
+            System.out.println(msg);
+
+            //fermeture de la connexion
+            dif.close();
+            c.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
---
+++ src/serveur/ServeurCaractere.java
@@ -0,0 +1,38 @@
+package serveur;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+import java.net.*;
+
+public class ServeurCaractere {
+
+    public static void main(String[] args) {
+        try {
+            // Declaration du serveur
+            ServerSocket s = new ServerSocket(8080);
+            // etablissement de la connexion
+            Socket c = s.accept();
+
+            // transmission
+            // client -> input => serveur->output
+            // serveur -> input <= client->output
+            OutputStream out = c.getOutputStream();
+            BufferedOutputStream bout = new BufferedOutputStream(out);
+            DataOutputStream dout = new DataOutputStream(bout);
+
+            dout.writeUTF("Bonjour le client");
+
+            dout.flush();
+
+            // fermeture de la connexion
+            dout.close();
+            c.close();
+            s.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
